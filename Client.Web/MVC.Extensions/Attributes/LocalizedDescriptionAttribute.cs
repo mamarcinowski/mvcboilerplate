b4bead5a5b9bae2bab6fa075b6bee12d0e538807
Mateusz Marcinowski <mateusz.marcinowski@gmail.com>
2015-08-30 21:28:39 +0200

    MVC.Extensions

---
+++ Client.Web/MVC.Extensions/Attributes/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Extensions.Attributes
+{
+    public class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        private readonly string _resourceKey;
+        private readonly ResourceManager _resource;
+        public LocalizedDescriptionAttribute(string resourceKey, Type resourceType)
+        {
+            _resource = new ResourceManager(resourceType);
+            _resourceKey = resourceKey;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                string displayName = _resource.GetString(_resourceKey);
+
+                return string.IsNullOrEmpty(displayName)
+                    ? string.Format("[[{0}]]", _resourceKey)
+                    : displayName;
+            }
+        }
+    }
+
+}
